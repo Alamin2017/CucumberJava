d3b9884d4a049548c94428f2f7c00fb91f066f6d
Al-Amin <alamincse12@gmail.com>
2023-08-03 23:38:19 +0600

    file is added

---
+++ src/test/java/StepDefinitions/RegistrationStepdefs.java
@@ -0,0 +1,114 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class RegistrationStepdefs {
+    public static WebDriver driver;
+    @Given("User go to the NopCommerce Home page")
+    public void userGoToTheNopCommerceHomePage() throws InterruptedException {
+        driver=new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+    }
+    @When("User click {string} option from {string} category")
+    public void userClickOptionFromCategory(String arg0, String arg1) throws InterruptedException {
+        Actions action=new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='Electronics']"))).perform();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//ul[@class='top-menu notmobile']//a[normalize-space()='Cell phones']")).click();
+    }
+    @And("User click on the {string} for product details")
+    public void userClickOnTheForProductDetails(String arg0) throws InterruptedException {
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//h2[@class='product-title']//a[contains(text(),'Nokia Lumia 1020')]")).click();
+    }
+    @And("User set the quantity number {int} in the quantity field")
+    public void userSetTheQuantityNumberInTheQuantityField(int arg0) {
+        driver.findElement(By.xpath("//input[@id='product_enteredQuantity_20']")).clear();
+        driver.findElement(By.xpath("//input[@id='product_enteredQuantity_20']")).sendKeys("2");
+    }
+    @And("User click on the {string} button")
+    public void userClickOnTheButton(String arg0) {
+        driver.findElement(By.xpath("//button[@id='add-to-cart-button-20']")).click();
+    }
+    @And("User go to the shipping cart page")
+    public void userGoToTheShippingCartPage() {
+        driver.findElement(By.xpath("//span[@class='cart-label']")).click();
+    }
+    @And("User accept terms conditions and click checkout button")
+    public void userAcceptTermsConditionsAndClickCheckoutButton() {
+        driver.findElement(By.xpath("//input[@id='termsofservice']")).click();
+        driver.findElement(By.xpath("//button[@id='checkout']")).click();
+    }
+    @And("User click checkout as guest button")
+    public void userClickCheckoutAsGuestButton() {
+        driver.findElement(By.xpath("//button[normalize-space()='Checkout as Guest']")).click();
+    }
+    @And("User input all the billing details and click continue")
+    public void userInputAllTheBillingDetailsAndClickContinue() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='BillingNewAddress_FirstName']")).sendKeys("Sumon");
+        driver.findElement(By.xpath("//input[@id='BillingNewAddress_LastName']")).sendKeys("Ahmed");
+        driver.findElement(By.xpath("//input[@id='BillingNewAddress_Email']")).sendKeys("sumon12@gmail.com");
+        WebElement element =driver.findElement(By.xpath("//select[@id='BillingNewAddress_CountryId']"));
+        Select se=new Select(element);
+        se.selectByVisibleText("Bangladesh");
+        driver.findElement(By.xpath("//input[@id='BillingNewAddress_City']")).sendKeys("Dhaka");
+        driver.findElement(By.xpath("//input[@id='BillingNewAddress_Address1']")).sendKeys("Khilkhet");
+        driver.findElement(By.xpath("//input[@id='BillingNewAddress_ZipPostalCode']")).sendKeys("1229");
+        driver.findElement(By.xpath("//input[@id='BillingNewAddress_PhoneNumber']")).sendKeys("01699604898");
+        driver.findElement(By.xpath("//button[@onclick='Billing.save()']")).click();
+    }
+
+    @And("User select shipping method {string} and click continue")
+    public void userSelectShippingMethodAndClickContinue(String arg0) throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='shippingoption_1']")).click();
+        driver.findElement(By.xpath("//button[@class='button-1 shipping-method-next-step-button']")).click();
+    }
+
+    @And("User select payment method {string} and click continue")
+    public void userSelectPaymentMethodAndClickContinue(String arg0) throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='paymentmethod_1']")).click();
+        driver.findElement(By.xpath("//button[@class='button-1 payment-method-next-step-button']")).click();
+    }
+
+    @And("User select {string} card and input card information")
+    public void userSelectCardAndInputCardInformation(String arg0) throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='CardholderName']")).sendKeys("sumon");
+        driver.findElement(By.xpath("//input[@id='CardNumber']")).sendKeys("4716946601381503");
+        driver.findElement(By.xpath("//input[@id='CardCode']")).sendKeys("650");
+        WebElement element=driver.findElement(By.xpath("//select[@id='ExpireYear']"));
+        Select drop=new Select(element);
+        drop.selectByVisibleText("2026");
+        driver.findElement(By.xpath("//button[@class='button-1 payment-info-next-step-button']")).click();
+    }
+
+    @And("User click confirm button to place the order")
+    public void userClickConfirmButtonToPlaceTheOrder() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[normalize-space()='Confirm']")).click();
+    }
+
+    @Then("Verify that the order place message {string} is d")
+    public void verifyThatTheOrderPlaceMessageIsD(String expectedMessage) throws InterruptedException {
+        Thread.sleep(2000);
+        String actualMessage=driver.findElement(By.xpath("//strong[normalize-space()='Your order has been successfully processed!']")).getText();
+        Assert.assertEquals(actualMessage,expectedMessage);
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
--- target/test-classes/Features/login.feature
+++ target/test-classes/Features/login.feature
@@ -1,12 +1,18 @@
-Feature: feature to test login functionality
+@registration
+Feature: Nopcommerce registration feature Test
 
-  Scenario Outline: Check login is successful with valid credentials
-    Given user is on login page
-    When user enters <username> and <password>
-    And clicks on login button
-    Then user is navigated to the home page
-
-    Examples: 
-      | username            | password |
-      | admin@yourstore.com | admin    |
-      | admin@yourstore.com | admin12  |
+  Scenario Outline: User should be able to registration new account successfully
+    Given User go to the NopCommerce Home page
+    And User navigate to the Registration page
+    When User select the <type> as gender
+    And User set first name and last name
+    And User set <dob> as date of birth
+    And User set <dynamicEmail> as email
+    And User set <companyName> as company details
+    And User set Newsletter option as <status>
+    And User set <password> as password and confirm password again
+    And User click on the Register button
+    Then Verify that the new account registration message <msg> is displayed
+    Examples:
+      | type     | dob          | dynamicEmail | companyName       | status      | password    | msg                           |
+      | "Male"   | "20/05/1995" | ""           | "Brainstation-23" | "checked"   | "pass@1234" | "Your registration completed" |
